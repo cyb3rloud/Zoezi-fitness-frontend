366ecc49fbd611e81d187ce647c42258fc9f1250
MercyChebet <chebetm96@gmail.com>
2023-01-12 15:32:43 +0300

    Adding stats

--- src/components/About.js
+++ src/components/About.js
@@ -9,3 +9,22 @@ function Section({ heading, body }) {
           </div>
     )
 }
+function Stat({ h, b }) {
+    return (
+          <div className="stat">
+                <span>{h}</span>
+                <p>{b}</p>
+          </div>
+    )
+}
+
+function Stats() {
+    return (
+          <div className="about-us-stats">
+                <Stat h="150K+" b="Users" />
+                <Stat h="350+" b="Training Exercises" />
+                <Stat h="20+" b="Trainers" />
+                <Stat h="50K+" b="Fitness Gear" />
+          </div>
+    )
+}
