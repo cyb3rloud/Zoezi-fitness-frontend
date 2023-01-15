50253bd445226019e88d09ef623473dfa417c0cd
JACKLINEKARIUKI <jacklinewk71@gmail.com>
2023-01-15 07:23:59 +0300

    add tables to the admin dashboard

---
+++ src/Dashboards/Data.js
@@ -0,0 +1,116 @@
+// Sidebar imports
+import {
+  UilUsersAlt,
+  UilClipboardAlt,
+  UilUserMd,
+  UilPackage,
+  UilChart,
+  UilSignOutAlt,
+} from "@iconscout/react-unicons";
+
+// Analytics Cards imports
+import { UilUsdSquare, UilMoneyWithdrawal } from "@iconscout/react-unicons";
+import { keyboard } from "@testing-library/user-event/dist/keyboard";
+
+// Recent Card Imports
+import img1 from "../images/img1.png";
+import img2 from "../images/img2.png";
+import img3 from "../images/img3.png";
+
+// Sidebar Data
+export const SidebarData = [
+  {
+    icon: UilUserMd,
+    heading: "Admin Dashboard",
+  },
+  {
+    icon: UilUsersAlt,
+    heading: "Users",
+  }
+];
+
+// Analytics Cards Data
+export const cardsData = [
+  {
+    title: "Users",
+    color: {
+      backGround: "linear-gradient(180deg, #4bb49b 0%, #6cf6d6 100%)",
+      boxShadow: "0px 10px 20px 0px #6cf6d6",
+    },
+    // barValue: 70,
+    value: "150",
+    png: UilUsersAlt,
+    users: [
+      {
+        name: "Pretty",
+        trainers: 2,
+      },
+      {
+        name: "Harvey",
+        trainers: 1,
+      },
+    ],
+  },
+  {
+    title: "Trainers",
+    color: {
+      backGround: "linear-gradient(180deg, #4bb49b 0%, #6cf6d6 100%)",
+      boxShadow: "0px 10px 20px 0px #6cf6d6",
+    },
+    // barValue: 80,
+    value: "30",
+    png: UilUsersAlt,
+    trainers: [
+      {
+        name: "Hanz",
+        users: 10,
+      },
+      {
+        name: "Harvey",
+        users: 5,
+      },
+    ],
+  },
+  {
+    title: "Workouts",
+    color: {
+      backGround: "linear-gradient(180deg, #4bb49b 0%, #6cf6d6 100%)",
+      boxShadow: "0px 10px 20px 0px #6cf6d6",
+    },
+    // barValue: 60,
+    value: "200",
+    png: UilUserMd,
+    workouts: [
+      {
+        name: "Abs",
+        trainers: 10,
+      },
+      {
+        name: "cardio",
+        trainers: 10,
+      },
+    ],
+  },
+];
+
+// Recent Update Card Data
+export const UpdatesData = [
+  {
+    img: img1,
+    name: "Andrew Thomas",
+    noti: "has ordered Apple smart watch 2500mh battery.",
+    time: "25 seconds ago",
+  },
+  {
+    img: img2,
+    name: "James Bond",
+    noti: "has received Samsung gadget for charging battery.",
+    time: "30 minutes ago",
+  },
+  {
+    img: img3,
+    name: "Iron Man",
+    noti: "has ordered Apple smart watch, samsung Gear 2500mh battery.",
+    time: "2 hours ago",
+  },
+];
---
+++ src/Dashboards/Table.js
@@ -0,0 +1,189 @@
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import "./Dashboard.css";
+
+function createData(name, trackingId, date, status) {
+  return { name, trackingId, date, status };
+}
+
+function createData1(name, trackingId, date, status, client_goal) {
+  return { name, trackingId, date, status, client_goal };
+}
+
+const rows = [
+  createData("Lasania Chiken Fri", 18908424, "2 March 2022", "Approved"),
+  createData("Big Baza Bang ", 18908424, "2 March 2022", "Pending"),
+  createData("Mouth Freshner", 18908424, "2 March 2022", "Approved"),
+];
+
+const users = [
+  createData1("DR. Stockmann", 38, "110kg", "+254712345678", "Lose Weight"),
+  createData1("DR. Stockmann", 38, "110kg", "+254712345678", "Lose Weight"),
+  createData1("DR. Stockmann", 38, "110kg", "+254712345678", "Lose Weight"),
+]
+
+const makeStyle = (status) => {
+  if (status === "Approved") {
+    return {
+      background: "rgb(145 254 159 / 47%)",
+      color: "green",
+    };
+  } else if (status === "Pending") {
+    return {
+      background: "#ffadad8f",
+      color: "red",
+    };
+  } else {
+    return {
+      background: "#59bfff",
+      color: "white",
+    };
+  }
+};
+
+export default function BasicTable() {
+  return (
+    <>
+      <div className="Tables">
+        <div className="table">
+          <h3>Users</h3>
+          <TableContainer
+            component={Paper}
+            style={{ boxShadow: "0px 13px 20px 0px #80808029" }}
+          >
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Name</TableCell>
+                  <TableCell align="left">Age</TableCell>
+                  <TableCell align="left">Weight</TableCell>
+                  <TableCell align="left">Contact</TableCell>
+                  <TableCell align="left">client_goal</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody style={{ color: "white" }}>
+                {users.map((user) => (
+                  <TableRow
+                    key={user.name}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    <TableCell component="th" scope="row">
+                      {user.name}
+                    </TableCell>
+                    <TableCell align="left">{user.trackingId}</TableCell>
+                    <TableCell align="left">{user.date}</TableCell>
+                    <TableCell align="left">
+                      <span className="status" style={makeStyle(user.status)}>
+                        {user.status}
+                      </span>
+                    </TableCell>
+                    <TableCell align="left" className="Details">
+                      {user.client_goal}
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <div className="t-button">
+            <button className="table-button">Add User</button>
+          </div>
+        </div>
+        <div className="table">
+          <h3>Trainers</h3>
+          <TableContainer
+            component={Paper}
+            style={{ boxShadow: "0px 13px 20px 0px #80808029" }}
+          >
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Name</TableCell>
+                  <TableCell align="left">Expertise</TableCell>
+                  <TableCell align="left">Clients</TableCell>
+                  <TableCell align="left">Sessions</TableCell>
+                  <TableCell align="left">Contact</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody style={{ color: "white" }}>
+                {users.map((user) => (
+                  <TableRow
+                    key={user.name}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    <TableCell component="th" scope="row">
+                      {user.name}
+                    </TableCell>
+                    <TableCell align="left">{user.trackingId}</TableCell>
+                    <TableCell align="left">{user.date}</TableCell>
+                    <TableCell align="left">
+                      <span className="status" style={makeStyle(user.status)}>
+                        {user.status}
+                      </span>
+                    </TableCell>
+                    <TableCell align="left" className="Details">
+                      {user.client_goal}
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <div className="t-button">
+            <button className="table-button">Add Trainers</button>
+          </div>
+        </div>
+        <div className="table">
+          <h3>Recent Orders</h3>
+          <TableContainer
+            component={Paper}
+            style={{ boxShadow: "0px 13px 20px 0px #80808029" }}
+          >
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Product</TableCell>
+                  <TableCell align="left">Tracking ID</TableCell>
+                  <TableCell align="left">Date</TableCell>
+                  <TableCell align="left">Status</TableCell>
+                  <TableCell align="left"></TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody style={{ color: "white" }}>
+                {rows.map((row) => (
+                  <TableRow
+                    key={row.name}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    <TableCell component="th" scope="row">
+                      {row.name}
+                    </TableCell>
+                    <TableCell align="left">{row.trackingId}</TableCell>
+                    <TableCell align="left">{row.date}</TableCell>
+                    <TableCell align="left">
+                      <span className="status" style={makeStyle(row.status)}>
+                        {row.status}
+                      </span>
+                    </TableCell>
+                    <TableCell align="left" className="Details">
+                      Details
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <div className="t-button">
+            <button className="table-button">Add Products</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
