9171e7a59196f3fe7633d3164225629cb306c4e6
JACKLINEKARIUKI <jacklinewk71@gmail.com>
2023-01-16 05:20:52 +0300

    create form for adding orders

---
+++ src/components/AddOrder.js
@@ -0,0 +1,135 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Container from "@material-ui/core/Container";
+import { toast } from "react-toastify";
+import axios from "axios";
+import { Input } from "@mui/material";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import Sidebar from "../Dashboards/Sidebar";
+
+// initial data state
+const initialState = {
+  product: "",
+  tracking_id: "",
+  date: "",
+  status: "",
+};
+
+const AddOrder = ({ AddOrder }) => {
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState(initialState);
+
+  const { product, tracking_id, date, status } = formData;
+
+  const handleChange = (e) => {
+    let { name, value } = e.target;
+
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!product || !tracking_id || !date || !status) {
+      console.log("please fill all input fields");
+      // toast.error("please fill all input fields");
+    } else {
+      // post comment to our api endpoint
+      // axios.post("http://localhost:4000/comments", formData);
+      fetch("url", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      })
+        .then((res) => res.json())
+        .then((data) => AddOrder(data));
+
+      // // show success message after post to the db
+      // toast.success("Added successfully");
+
+      // restore input fields to default
+      setFormData({
+        product: "",
+        tracking_id: "",
+        date: "",
+        status: "",
+      });
+      console.log("hello");
+      // navigate back to testimonials page
+      navigate("/Dashboard");
+
+      // call product render function
+      //   setTimeout(() => loadProduct(), 500);
+    }
+  };
+
+  return (
+    <>
+      <Navbar />
+      <div className="forms">
+        <Sidebar />
+        <div className="AddItem">
+          <Container>
+            <div className="AddContainer">
+              <h1>Add Order</h1>
+              <form onSubmit={handleSubmit}>
+                <div>
+                  <label> Product </label> <br />
+                  <input
+                    type="text"
+                    name="product"
+                    value={formData.product}
+                    onChange={handleChange}
+                  />
+                  <br />
+                  <label>Tracking ID:</label>
+                  <textarea
+                    type="number"
+                    name="tracking_id"
+                    value={formData.tracking_id}
+                    onChange={handleChange}
+                  />
+                  <br />
+                  <label>Date:</label>
+                  <br />
+                  <input
+                    type="date"
+                    name="date"
+                    value={formData.date}
+                    onChange={handleChange}
+                  />
+                  <br />
+                  <label>Status:</label>
+                  <select  className="drop-down" name="status">
+                    <option value="approved"></option>
+                    <option value="approved">Approved</option>
+                    <option value="pending">Pending</option>
+                  </select>
+                  <br />
+                </div>
+                <div className="add-button">
+                  <button
+                    className="p-button"
+                    variant="contained"
+                    type="submit"
+                  >
+                    Add
+                  </button>
+                </div>
+              </form>
+            </div>
+          </Container>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default AddOrder;
