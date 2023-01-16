024c4cfc27aea04e8ba22b27f69357d2379d7037
Titus Mumo <titusmumo@gmail.com>
2023-01-16 21:40:49 +0300

    login

--- src/components/Login.js
+++ src/components/Login.js
@@ -12,7 +12,7 @@ function Login() {
         // Fetch data from backend
         async function fetchData() {
             try {
-                const response = await fetch('http://127.0.0.1:3000/login', {
+                const response = await fetch('/login', {
                     method: 'POST',
                     headers: { 'Content-Type': 'application/json' },
                     body: JSON.stringify({ email, password }),
@@ -22,7 +22,7 @@ function Login() {
                     // Save user data to local storage
                     localStorage.setItem('clients', JSON.stringify(data.clients));
                     // Redirect to dashboard
-                    navigate.push("/dashboard");
+                    navigate("/dashboard");
                 } else {
                     setError(data.message);
                 }
