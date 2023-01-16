1ccc26a1467ad8ce0016b605d0a04b22c144f38d
JACKLINEKARIUKI <jacklinewk71@gmail.com>
2023-01-16 05:23:44 +0300

    modified routing for the pages

--- src/App.js
+++ src/App.js
@@ -13,6 +13,15 @@ import Register from "./components/Register";
 import Testimonialss from "./components/Testimonialss";
 import Dashboard from "./Dashboards/Dashboard";
 import UserDashboard from "./Dashboards/UserDashboard";
+import AddProduct from "./components/AddProduct";
+import AddUser from "./components/AddUser";
+import AddTrainer from "./components/AddTrainer";
+import AddOrder from "./components/AddOrder";
+import AllUsers from "./components/AllUsers";
+import AllTrainers from "./components/AllTrainers";
+import AllProducts from "./components/AllProducts";
+import AllOrders from "./components/AllOrders";
+
 
 function App() {
   return (
@@ -29,6 +38,15 @@ function App() {
           <Route exact path="/Testimonialss" element={<Testimonialss />} />
           <Route exact path="/Dashboard" element={<Dashboard />} />
           <Route exact path="/UserDashboard" element={<UserDashboard />} />
+          <Route exact path="/AddProduct" element={<AddProduct />} />
+          <Route exact path="/AddUser" element={<AddUser />} />
+          <Route exact path="/AddTrainer" element={<AddTrainer />} />
+          <Route exact path="/AddOrder" element={<AddOrder />} />
+          <Route exact path="/AllUsers" element={<AllUsers />} />
+          <Route exact path="/AllTrainers" element={<AllTrainers />} />
+          <Route exact path="/AllProducts" element={<AllProducts />} />
+          <Route exact path="/AllOrders" element={<AllOrders />} />
+
         </Routes>
 
         {/* <div style={{overflowY:"scroll", height:"800px"}}></div>  */}
