e13ae04d1f7209907bda26517eeb90968f855c52
Titus Mumo <titusmumo@gmail.com>
2023-01-19 18:03:38 +0300

    linking backend ro trainers page

--- src/components/Trainer.js
+++ src/components/Trainer.js
@@ -13,7 +13,7 @@ function Profile({ trainer, handleClick }) {
       return (
         <div className="all-trainers-profile" onClick={handleClick}>
           <div className="all-trainers-profile-img">
-            <img src="https://racheltrotta.com/wp-content/uploads/2019/10/extraLR6A0475-1024x683.jpg" alt="trainer" />
+            <img src={trainer.image_url} alt="trainer" />
           </div>
           <span>{trainer.firstname} {trainer.lastname}</span>
           <div>
@@ -36,7 +36,7 @@ function Profile({ trainer, handleClick }) {
       const [trainers, setTrainers] = useState([]);
       const [error, setError] = useState(null);
       useEffect(() => {
-        fetch("https://8b3e76f0-d564-4e08-a73c-2eca5d1665d2.mock.pstmn.io/trainers")
+        fetch(`/trainers`)
           .then(res => {
             if (!res.ok) {
               throw new Error("Error occured while fetching the data!");
--- src/components/Trainerinfo.js
+++ src/components/Trainerinfo.js
@@ -5,7 +5,7 @@ import './Trainerinfo.css'
 // import Footer from "./Footer"
 
 
-function User({ trainer, workouts, exercises }) {
+function User({ trainer, workouts, schedules }) {
   return (
     <div className="main">
       <div className="entry-container">
@@ -13,7 +13,7 @@ function User({ trainer, workouts, exercises }) {
       <div className="e-container">
         <div className="top-header">
           <div>
-            <img className="image" src="https://racheltrotta.com/wp-content/uploads/2019/10/extraLR6A0475-1024x683.jpg"/>
+            <img className="image" src={trainer.image_url}/>
           </div>
           <div className="name">{trainer.firstname} {trainer.lastname}</div>
           <div className="sub-name">Pro Trainer</div>
@@ -21,22 +21,24 @@ function User({ trainer, workouts, exercises }) {
           <div className="btn-container">
             <button className="btnx">Add to Dashboard</button> 
             <div className="tags">
-              {workouts.map((workout, index) => (
+              {workouts?.map((workout, index) => (
                 <button className="btns" key={index}>{workout.workout_name}</button>
               ))}
             </div>
           </div>
           <div className="cards">
-            {exercises.map((exercise, index) => (
+            {schedules?.map((schedule, index) => (
               <div class="exercises">
-                <div class="card-header">Exercise</div>
-                <div class="card-text">{exercise.exercise_name}</div> 
-                <div class="card-header">Activity</div>
-                <div class="card-text">{exercise.activity}</div>
-                <div class="card-header">Muscle Group</div>
-                <div class="card-text">{exercise.muscle_group}</div> 
-                <div class="card-header">Time</div>
-                <div class="card-text">{exercise.time_start} to {exercise.time_end}</div> 
+                <div class="card-header">session</div>
+                <div class="card-text">{schedule.session}</div> 
+                <div class="card-header">From:</div>
+                <div class="card-text">{schedule.session_start}</div>
+                <div class="card-header">To:</div>
+                <div class="card-text">{schedule.session_end}</div> 
+                <div class="card-header">Date</div>
+                <div class="card-text">{schedule.date}</div> 
+                {/* <div class="card-header">Exercises</div>
+                <div class="card-text">{console.log(schedule.exercises)}</div>  */}
               </div>
             ))}
           </div>
@@ -55,7 +57,7 @@ function Trainerinfo() {
 
   useEffect(() => {
     // Fetch trainer data
-    fetch("https://8b3e76f0-d564-4e08-a73c-2eca5d1665d2.mock.pstmn.io/trainers")
+    fetch(`/trainers/${id}`)
       .then(res => {
         if (!res.ok) {
           throw new Error("Error occured while fetching the trainer data!");
@@ -63,33 +65,38 @@ function Trainerinfo() {
         return res.json();
       })
       .then(data => {
-        setTrainers(data[0]);
-        // Fetch workout data
-        return fetch(`https://fe1d58d4-b2fe-46b6-a2e7-941a9003218c.mock.pstmn.io/workouts`)
+        setTrainers(data);
       })
-      .then(res => {
-        if (!res.ok) {
-          throw new Error("Error occured while fetching the workout data!");
-        }
-        return res.json();
-      })
-      .then(workoutData => {
-        setWorkouts(workoutData);
-        // Fetch exercise data
-        return fetch('https://7aa1a781-6a90-42da-b382-7e585c7da80a.mock.pstmn.io/exercises')
-      })
-      .then(res => {
-        if (!res.ok) {
-          throw new Error("Error occured while fetching the exercise data!");
-        }
-        return res.json();
-      })
-      .then(exerciseData => {
-        setExercises(exerciseData);
-      })
-      .catch(error => {
-        setError(error);
-      });
+  
+      // .then(data => {
+      //   console.log(data);
+      //   setTrainers(data);
+      //   // Fetch workout data
+      //   return fetch(`/workouts`)
+      // })
+      // .then(res => {
+      //   if (!res.ok) {
+      //     throw new Error("Error occured while fetching the workout data!");
+      //   }
+      //   return res.json();
+      // })
+      // .then(workoutData => {
+      //   setWorkouts(workoutData);
+      //   // Fetch exercise data
+      //   return fetch('/exercises')
+      // })
+      // .then(res => {
+      //   if (!res.ok) {
+      //     throw new Error("Error occured while fetching the exercise data!");
+      //   }
+      //   return res.json();
+      // })
+      // .then(exerciseData => {
+      //   setExercises(exerciseData);
+      // })
+      // .catch(error => {
+      //   setError(error);
+      // });
     }, []);
     
     if (error) {
@@ -103,7 +110,7 @@ function Trainerinfo() {
     return (
       <div>
         {/* <Navbar /> */}
-        <User trainer={trainers} workouts={workouts} exercises={exercises} />
+        <User trainer={trainers} workouts={ trainers.workouts} schedules={trainers.schedules} />
         {/* <Footer /> */}
       </div>
     );
